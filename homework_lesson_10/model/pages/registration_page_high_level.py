41be9f0721b4ce1c8ee72b8e8464018b7052b09f
flowered_pants <rikydo@mail.ru>
2023-04-24 16:18:27 +0530

    ...

--- homework_lesson_10/model/pages/registration_page_high_level.py
+++ homework_lesson_10/model/pages/registration_page_high_level.py
@@ -1,12 +1,11 @@
 from selene import browser
 from selene import have, be
 from selenium.webdriver.common.keys import Keys
-
 from homework_lesson_10 import resources
+from homework_lesson_10.data.users import Students
 
 
 class RegistrationPage:
-
     def open(self):
         browser.open('/automation-practice-form')
         browser.should(have.title('DEMOQA'))
@@ -14,19 +13,19 @@ class RegistrationPage:
         browser.element('footer').execute_script('element.remove()')
         browser.element('.sidebar-content').execute_script('element.remove()')
 
-    def register(self, first_name, last_name, mail, gender, number, year, month, day, address):
-        browser.element('#firstName').should(be.blank).type(first_name)
-        browser.element('#lastName').should(be.blank).type(last_name)
-        browser.element('#userEmail').should(be.blank).type(mail)
+    def register(self, student):
+        browser.element('#firstName').should(be.blank).type(student.first_name)
+        browser.element('#lastName').should(be.blank).type(student.last_name)
+        browser.element('#userEmail').should(be.blank).type(student.email)
         browser.element('[for="gender-radio-2"]').should(be.clickable).click()
-        browser.element('#userNumber').should(be.blank).type(number)
+        browser.element('#userNumber').should(be.blank).type(student.number)
         browser.driver.execute_script('window.scrollBy(0, 300)')
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__year-select').click()
-        browser.element(f'option[value="{year}"]').click()
+        browser.element(f'option[value="{student.year}"]').click()
         browser.element('.react-datepicker__month-select').click()
-        browser.element(f'option[value="{month}"]').click()
-        browser.element(f'.react-datepicker__day--0{day}').click()
+        browser.element(f'option[value="{student.month}"]').click()
+        browser.element(f'.react-datepicker__day--0{student.day}').click()
         browser.element('#subjectsInput').click()
         browser.element('#subjectsInput').send_keys('Computer Science')
         browser.element('#subjectsInput').press(Keys.TAB)
@@ -34,7 +33,7 @@ class RegistrationPage:
         browser.element('[for="hobbies-checkbox-2"]').should(be.clickable).click()
         browser.element('[for="hobbies-checkbox-3"]').should(be.clickable).click()
         browser.element('#uploadPicture').send_keys(resources.path())
-        browser.element('#currentAddress').should(be.blank).type(address)
+        browser.element('#currentAddress').should(be.blank).type(student.address)
         browser.driver.execute_script('window.scrollBy(0, 500)')
         browser.element('#state').click()
         browser.element('#react-select-3-option-1').click()
@@ -42,10 +41,12 @@ class RegistrationPage:
         browser.element('#react-select-4-option-2').click()
         browser.element('#submit').click()
 
-    def should_have_registered(self, first_name, last_name, email, gender, number, birthday,
-                                      subject, hobbies, picture, address, state_and_city):
+    def should_have_registered(self, student):
         browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-        browser.element('.table').all('td').even.should(have.exact_texts(first_name, last_name, email, gender, number,
-                                                                         birthday, subject, hobbies, picture,
-                                                                         address, state_and_city))
-
+        browser.element('.table').all('td').even.should(have.exact_text(repr(student.first_name + ' ' + student.last_name,
+                                                                         student.email, student.gender,
+                                                                         student.number,
+                                                                         student.day + ' February' + ',' + student.year,
+                                                                         student.subject, student.hobbies,
+                                                                         student.picture,
+                                                                         student.address, student.state_and_city)))
--- homework_lesson_10/tests/registration_test.py
+++ homework_lesson_10/tests/registration_test.py
@@ -1,11 +1,7 @@
-from homework_lesson_10.data import users
-from homework_lesson_10.data.users import Students
+from homework_lesson_10.data.users import student
 from homework_lesson_10.model.pages.registration_page_high_level import RegistrationPage
 
-student = Students(first_name='Evgeniia', last_name='Belikova', email='mur@loc.ru',
-                   gender='Female', number='88002000600', birthday='27 March,1997', subject='Computer Science',
-                   hobbies='Sports, Reading, Music', picture='pic.png', address='Moscow, Russia',
-                   state_and_city='Uttar Pradesh Merrut')
+
 
 def test_registration_form(browser_managment):
     registration_page = RegistrationPage()
